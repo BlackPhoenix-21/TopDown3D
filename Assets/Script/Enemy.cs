7c4e09445055d74a635be2ecda17eb7493b91e9e
Sören Irlenbusch <176750049+BlackPhoenix-21@users.noreply.github.com>
2025-09-10 12:32:14 +0200

    AnimationFix

--- Assets/Script/Enemy.cs
+++ Assets/Script/Enemy.cs
@@ -7,15 +7,26 @@ public class Enemy : MonoBehaviour
     NavMeshAgent agent;
     public NavMeshPath path;
     public Spawner spawner;
+    private Animator animator;
 
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
         bool pathset = agent.SetPath(path);
     }
 
     void Update()
     {
+        if (agent.velocity.magnitude > 0)
+        {
+            animator.SetBool("moving", true);
+        }
+        else
+        {
+            animator.SetBool("moving", false);
+        }
+
         if (agent.remainingDistance < 0.5 && path != null)
         {
             agent.SetPath(spawner.EndPoint(path));
--- Assets/Script/Player.cs
+++ Assets/Script/Player.cs
@@ -4,24 +4,35 @@ using UnityEngine.AI;
 public class Player : MonoBehaviour
 {
     private NavMeshAgent navMeshAgent;
+    private Animator animator;
 
     void Start()
     {
         navMeshAgent = GetComponent<NavMeshAgent>();
+        animator = GetComponent<Animator>();
     }
 
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            // navMeshAgent.SetDestination(new Vector3(Random.Range(5f, -5f), 0, Random.Range(5f, -5f)));
-
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hit;
             if (Physics.Raycast(ray, out hit))
             {
                 navMeshAgent.SetDestination(hit.point);
             }
+        }
+
+        if (navMeshAgent.velocity.magnitude > 0)
+        {
+            animator.SetBool("moving", true);
+            animator.SetFloat("speed", navMeshAgent.velocity.magnitude);
+        }
+        else
+        {
+            animator.SetBool("moving", false);
+            animator.SetFloat("speed", navMeshAgent.velocity.magnitude);
         }
     }
 }
